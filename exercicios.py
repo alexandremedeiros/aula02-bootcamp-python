04e3e733a30a7256e56d11b5a76cfa7db9b6d7c8
Alexandre Medeiros <alexandremedeiros@MacBook-Air-de-Alexandre.local>
2024-07-20 13:56:07 -0300

    Uso de try/except e split de string

--- exercicios.py
+++ exercicios.py
@@ -5,3 +5,24 @@ import math
 raio = float(input("Informe o raio do círculo: "))
 area = math.pi * raio ** 2
 print(f"{area:.2f}")
+
+
+# Divisão de inteiros
+try:
+    v1 = int(input("Informe o valor 1: "))
+    v2 = int(input("Informe o valor 2: "))
+    resultado = v1 // v2
+    print(resultado)
+except Exception as e:
+    print(e)
+
+
+#Separa o dia, mês e ano de uma data no formato dd/mm/yyyy e imprime
+try:
+    data = input("Informe uma data (dd/mm/aaaa): ")
+    data_separada = data.split("/")
+    print("Data separada")
+    for dado in data_separada:
+        print(dado)
+except Exception as e:
+    print(e)
